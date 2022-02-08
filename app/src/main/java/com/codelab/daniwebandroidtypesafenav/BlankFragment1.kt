1ea1fa15a0fdeb14a0223e60b78f5b146ea5e5ec
Dimitri Nguyen <dmitrilc@users.noreply.github.com>
2022-02-08 17:27:06 -0500

    Initial commit

---
+++ app/src/main/java/com/codelab/daniwebandroidtypesafenav/BlankFragment1.kt
@@ -0,0 +1,82 @@
+package com.codelab.daniwebandroidtypesafenav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment1 : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        val button = view.findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            //Will compile, but will crash at runtime. No Compiler checks (except for @IdRes).
+            //navController.navigate(R.id.button)
+
+            //Will compile. Won't crash at runtime. No Compiler checks (except for @IdRes).
+            //navController.navigate(R.id.action_blankFragment1_to_blankFragment2)
+
+            //Will compile. Won't crash at runtime. Has Compiler checks
+            //val direction = BlankFragment1Directions.actionBlankFragment1ToBlankFragment2()
+            val direction = BlankFragment1Directions.actionBlankFragment1ToBlankFragment2("Arg from fragment1 to fragment2")
+            navController.navigate(direction)
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment1.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BlankFragment1().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
